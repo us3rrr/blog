74421c549a80688d1fa2809ba2b65e476f06b7aa
us3rrr <43823591+us3rrr@users.noreply.github.com>
2018-10-03 16:37:32 -0300

    mudanca

--- rest_blog/api/categoria/create.php
+++ rest_blog/api/categoria/create.php
@@ -1,5 +1,5 @@
 <?php
-	header('Content-Type: application/json');
+	header('Content-Type: application/json, charset=utf-8');
 	
 	require_once '../../config/Conexao.php';
 	require_once '../../models/Categoria.php';
